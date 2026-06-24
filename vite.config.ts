108423799665b37d7c185d8eeeae91f32914f819
Hakuman710 <3139044483@qq.com>
2026-06-25 00:15:38 +0800

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  base: '/jiuguantext/',
+  build: { outDir: 'docs' },
+  plugins: [react(), tailwindcss()],
+});
